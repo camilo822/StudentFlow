c8fe568cc00cdd2a9f7295bb2015426ac13b2133
camilo <estudio5654@gmail.com>
2026-03-12 17:58:57 -0500

    se actalizo el codigo para poder usar google fonts

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../constants/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   AppTheme._();
@@ -13,19 +14,21 @@ class AppTheme {
         surface: AppColors.surface,
       ),
       scaffoldBackgroundColor: AppColors.background,
-      fontFamily: 'Poppins',
-      appBarTheme: const AppBarTheme(
+      
+      fontFamily: GoogleFonts.poppins().fontFamily,
+
+      appBarTheme: AppBarTheme( // <-- SE QUITO EL 'const' AQUÍ
         backgroundColor: AppColors.background,
         elevation: 0,
         scrolledUnderElevation: 0,
-        titleTextStyle: TextStyle(
-          fontFamily: 'Poppins',
+        titleTextStyle: GoogleFonts.poppins( // <-- SE USO GoogleFonts.poppins() directamente
           fontWeight: FontWeight.w600,
           fontSize: 20,
           color: AppColors.textPrimary,
         ),
-        iconTheme: IconThemeData(color: AppColors.textPrimary),
+        iconTheme: const IconThemeData(color: AppColors.textPrimary),
       ),
+      
       cardTheme: CardThemeData(
         color: AppColors.surface,
         elevation: 0,
@@ -33,31 +36,27 @@ class AppTheme {
           borderRadius: BorderRadius.circular(16),
         ),
       ),
-      textTheme: const TextTheme(
-        displayLarge: TextStyle(
-          fontFamily: 'Poppins',
+
+      textTheme: TextTheme( // <-- SE QUITO EL 'const' AQUÍ
+        displayLarge: GoogleFonts.poppins( // <-- SE CAMBIO TextStyle por GoogleFonts.poppins
           fontWeight: FontWeight.w700,
           color: AppColors.textPrimary,
         ),
-        titleLarge: TextStyle(
-          fontFamily: 'Poppins',
+        titleLarge: GoogleFonts.poppins(
           fontWeight: FontWeight.w600,
           fontSize: 18,
           color: AppColors.textPrimary,
         ),
-        titleMedium: TextStyle(
-          fontFamily: 'Poppins',
+        titleMedium: GoogleFonts.poppins(
           fontWeight: FontWeight.w600,
           fontSize: 16,
           color: AppColors.textPrimary,
         ),
-        bodyMedium: TextStyle(
-          fontFamily: 'Poppins',
+        bodyMedium: GoogleFonts.poppins(
           fontSize: 14,
           color: AppColors.textSecondary,
         ),
-        bodySmall: TextStyle(
-          fontFamily: 'Poppins',
+        bodySmall: GoogleFonts.poppins(
           fontSize: 12,
           color: AppColors.textSecondary,
         ),
@@ -74,19 +73,21 @@ class AppTheme {
         surface: AppColors.surfaceDark,
       ),
       scaffoldBackgroundColor: AppColors.backgroundDark,
-      fontFamily: 'Poppins',
-      appBarTheme: const AppBarTheme(
+      
+      fontFamily: GoogleFonts.poppins().fontFamily,
+
+      appBarTheme: AppBarTheme( // <-- SE QUITO EL 'const'
         backgroundColor: AppColors.backgroundDark,
         elevation: 0,
         scrolledUnderElevation: 0,
-        titleTextStyle: TextStyle(
-          fontFamily: 'Poppins',
+        titleTextStyle: GoogleFonts.poppins( // <-- SE USO GoogleFonts
           fontWeight: FontWeight.w600,
           fontSize: 20,
           color: AppColors.textPrimaryDark,
         ),
-        iconTheme: IconThemeData(color: AppColors.textPrimaryDark),
+        iconTheme: const IconThemeData(color: AppColors.textPrimaryDark),
       ),
+      
       cardTheme: CardThemeData(
         color: AppColors.surfaceDark,
         elevation: 0,
