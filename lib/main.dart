bd588d4f721c1cfb862c3001f003d5e62bd9a086
camilo <estudio5654@gmail.com>
2026-03-25 12:12:28 -0500

    se inicializaron plugins y ahora carga shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'data/services/local_storage_service.dart';
 
-void main() {
-  runApp(const StudentFlowApp());
+void main() async {
+  // Necesario para inicializar plugins antes de runApp
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Carga shared_preferences una sola vez al iniciar
+  final storage = await LocalStorageService.getInstance();
+
+  runApp(StudentFlowApp(storage: storage));
 }
