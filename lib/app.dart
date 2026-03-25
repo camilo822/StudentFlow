3dfc9c8b71a39206e87b90449f31728c244a0b86
camilo <estudio5654@gmail.com>
2026-03-25 12:14:08 -0500

    Actualizacion del archivo

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
-import 'features/tasks/screens/home_screen.dart';
+import 'data/services/local_storage_service.dart';
 
 class StudentFlowApp extends StatelessWidget {
-  const StudentFlowApp({super.key});
+  final LocalStorageService storage;
+
+  const StudentFlowApp({super.key, required this.storage});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +16,8 @@ class StudentFlowApp extends StatelessWidget {
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.system,
-      home: const HomeScreen(),
+      initialRoute: AppRouter.initialRoute(storage),
+      onGenerateRoute: AppRouter.generateRoute,
     );
   }
 }
